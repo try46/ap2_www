0c53979dfc2a00754fafa4d5b5a70bbf5b0da06f
try46 <popuman61@gmail.com>
2017-12-05 16:21:28 +0900

    第10回講義

--- src/java/kougi10/Ex102.java
+++ src/java/kougi10/Ex102.java
@@ -1,13 +1,18 @@
-package kougi09;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package kougi10;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,12 +23,11 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author try
  */
-@WebServlet(urlPatterns = {"/kougi09/Ex91"})
-public class Ex91 extends HttpServlet {
+@WebServlet(name = "Ex102", urlPatterns = {"/kougi10/Ex102"})
+public class Ex102 extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      *
      * @param request servlet request
      * @param response servlet response
@@ -33,27 +37,69 @@ public class Ex91 extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        Connection con = null;
+        Statement stmt = null;
+        PreparedStatement ps=null;
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Ex91</title>");            
+            out.println("<title>Servlet Ex102</title>");
             out.println("</head>");
             out.println("<body>");
             out.println(
-                    "<h1>Servlet Ex91 at " + request.getContextPath() + "</h1>");
-            request.setCharacterEncoding("UTF-8");
-            out.println("<table border=\"1\">");
-            String year=request.getParameter("year");
-            out.println("<tr><td>氏名</td><td>"+year+"</td></tr>");
-              String month=request.getParameter("month");
-             out.println("<tr><td>住所</td><td>"+month+"</td></tr>");
-              String day=request.getParameter("day");
-               out.println("<tr><td>興味</td><td>"+day+"</td></tr>");
-               out.println("</table>");
+                    "<h1>Servlet Ex101 at " + request.getContextPath() + "</h1>");
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            String driverUrl = "jdbc:derby://localhost:1527/meibo";
+            con = DriverManager.getConnection(driverUrl, "db", "db");
+            stmt = con.createStatement();
+            //sqlの発行(挿入)
+            /*
+            String sql2 = "insert into T_STUDENT_A"
+                    + "(fullname,gakubu_id,grade)"
+                    + "values('松田聖子',1,4)";
+            int count = stmt.executeUpdate(sql2);
+            */
+            //sql文の発行
+            String sid=request.getParameter("sid");
+            String sql = "select * from T_STUDENT_A where student_id=?";
+            ps=con.prepareStatement(sql);
+            ps.setInt(1, Integer.parseInt(sid));
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                out.println("STUDENT_ID=" + rs.getInt("STUDENT_ID") + "<br>");
+                out.println("FULLNAME=" + rs.getString("FULLNAME") + "<br>");
+                out.println("GAKUBU_ID=" + rs.getInt("GAKUBU_ID") + "<br>");
+                out.println("GRADE=" + rs.getInt("GRADE") + "<hr>");
+            }
+            rs.close();
             out.println("</body>");
             out.println("</html>");
+        }catch(Exception e){
+            throw new ServletException(e);
+        }finally{
+            if (ps!=null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    throw new ServletException(e);
+                }
+            }
+                 if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException es) {
+                    throw new ServletException(es);
+                }
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    throw new ServletException(e);
+                }
+            }
         }
     }
 
