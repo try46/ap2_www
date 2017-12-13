c662ad1391ce3c4815920098c426b87264367e4f
try46 <popuman61@gmail.com>
2017-12-13 18:28:42 +0900

    第10回演習応用課題

--- src/java/en10/Kadai10_42.java
+++ src/java/en10/Kadai10_42.java
@@ -24,8 +24,8 @@ import javax.servlet.http.HttpSession;
  *
  * @author try
  */
-@WebServlet(name = "Kadai10_41", urlPatterns = {"/en10/Kadai10_41"})
-public class Kadai10_41 extends HttpServlet {
+@WebServlet(name = "Kadai10_42", urlPatterns = {"/en10/Kadai10_42"})
+public class Kadai10_42 extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -46,38 +46,34 @@ public class Kadai10_41 extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Kadai10_41</title>");
+            out.println("<title>Servlet Kadai10_42</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Kadai10_41 at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet Kadai10_42 at " + request.getContextPath() + "</h1>");
             Class.forName("org.apache.derby.jdbc.ClientDriver");
             String driverUrl = "jdbc:derby://localhost:1527/twitter";
             con = DriverManager.getConnection(driverUrl, "db", "db");
             stmt = con.createStatement();
-             request.setCharacterEncoding("UTF-8");
-            String tid = request.getParameter("tid");
-            String pass = request.getParameter("pas");
-            String sql = "select * from T_MEMBER where TWITTERID = ? and PASSWORD = ?";
-            ps = con.prepareStatement(sql);
-            ps.setString(1, tid);
-            ps.setString(2, pass);
-
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                out.println("IDとパスワードが一致しました"+"<br>");
-                HttpSession session=request.getSession(false);
-                if (session==null) {
-                    session=request.getSession(true);
-                    session.setAttribute("id", tid);
-                }else{
-                    
+            request.setCharacterEncoding("UTF-8");
+            HttpSession session = request.getSession(false);
+            if (session == null) {
+                out.println("<p>セッションがありません</p>");
+            } else {
+                out.println("twitterid=" + session.getAttribute("id")+"<hr>");
+                
+                String sql = "select * from T_TWEET where TWITTERID = ?";
+                ps = con.prepareStatement(sql);
+                ps.setString(1, (String)session.getAttribute("id"));
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    out.println("TWEETNO=" + rs.getInt("TWEETNO") + "<br>");
+                    out.println("TWITTERID=" + rs.getString("TWITTERID") + "<br>");
+                    out.println("TWEETTEXT=" + rs.getString("TWEETTEXT") + "<br>");
+                    out.println("IINE=" + rs.getInt("IINE") + "<hr>");
                 }
-                out.println("<p><a href=\"Kadai10_42\">該当IDのツイートを表示</a></p>");
-            }else{
-                out.println("IDとパスワードが一致しません!"+"<br>");
-                 out.println("<p><a href=\"Kadai10_4.html\">ログインページに戻る</a></p>");
+                rs.close();
             }
-            rs.close();
+
             out.println("</body>");
             out.println("</html>");
         } catch (Exception e) {
@@ -105,7 +101,6 @@ public class Kadai10_41 extends HttpServlet {
                 }
             }
         }
-
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
